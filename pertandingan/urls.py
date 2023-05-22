20a0cbc999e4b147f7717a3c2f3aa1f03cbee2da
annisa27 <annisa.az@ui.ac.id>
2023-05-22 16:44:43 +0700

    startapp pertandingan

---
+++ pertandingan/urls.py
@@ -0,0 +1,8 @@
+from django.urls import path
+# from pertandingan.views import show_catalog
+
+app_name = 'pertandingan'
+
+# urlpatterns = [
+#     path('', show_catalog, name='show_catalog'),
+# ]
